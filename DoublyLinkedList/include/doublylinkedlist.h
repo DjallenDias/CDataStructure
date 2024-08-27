fe9cce22fe399e01f0d8fb03d970b4dfe639c1e3
Djállen Fabrício <djallendias@gmail.com>
2024-08-27 14:31:41 -0300

    Created Doubly Linked List struct

--- DoublyLinkedList/include/doublylinkedlist.h
+++ DoublyLinkedList/include/doublylinkedlist.h
@@ -1,4 +1,6 @@
 #ifndef DOUBLYLINKEDLIST_H
 #define DOUBLYLINKEDLIST_H
 
+typedef struct doubly_linked_list DoublyList;
+
 #endif // DOUBLYLINKEDLIST_H
--- DoublyLinkedList/src/doublylinkedlist.c
+++ DoublyLinkedList/src/doublylinkedlist.c
@@ -1,1 +1,7 @@
 #include "doublylinkedlist.h"
+
+struct doubly_linked_list {
+    struct doubly_linked_list *prev;
+    int data;
+    struct doubly_linked_list *next;
+};
