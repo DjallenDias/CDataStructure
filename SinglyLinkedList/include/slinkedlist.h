8a6ef7dcc7e92b9a9f732abeac1c38d56c54f9dd
Djállen Fabrício <djallendias@gmail.com>
2024-08-27 13:16:53 -0300

    Header added new function def.

--- SinglyLinkedList/include/slinkedlist.h
+++ SinglyLinkedList/include/slinkedlist.h
@@ -15,11 +15,17 @@ List *concat(List *, List *);
 
 List *remove_n(List *, int);
 
+List *separates_in_n(List *, int);
+
+List *merge_lists(List *, List *);
+
 void insert_beginning(List **, int);
 
 void show_list(List *);
 
 void free_list(List **);
+
+int is_equal(List *, List *);
 
 int greater_than(List *, int);
 
