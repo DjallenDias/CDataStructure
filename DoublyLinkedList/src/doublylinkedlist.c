355edcb44c43e3f700b566197c736e0ed1ee518d
Djállen Fabrício <djallendias@gmail.com>
2024-09-20 23:01:27 -0300

    Created insert beginning/end functions - doubly linked list

--- DoublyLinkedList/src/doublylinkedlist.c
+++ DoublyLinkedList/src/doublylinkedlist.c
@@ -1,7 +1,49 @@
 #include "doublylinkedlist.h"
 
+#include <stdlib.h>
+#include <stdio.h>
 struct doubly_linked_list {
     struct doubly_linked_list *prev;
     int data;
     struct doubly_linked_list *next;
 };
+
+DoublyList *create_list(void) {
+    return NULL;
+}
+
+void insert_beginning(DoublyList **l, int n) {
+    DoublyList *aux = *l;
+    DoublyList *new = (DoublyList *) malloc(sizeof(DoublyList));
+
+    if (new == NULL) {
+        printf("Unnable to allocate memory");
+        return;
+    }
+
+    new->data = n;
+    new->next = aux->next;
+    new->prev = aux->prev;
+
+    *l = new;
+}
+
+void insert_end(DoublyList **l, int n) {
+    if ((*l) == NULL) {
+        insert_beginning(l, n);
+        return;
+    }
+
+    DoublyList *last = last_item(*l);
+    DoublyList *new = (DoublyList *) malloc(sizeof(DoublyList));
+
+    if (new == NULL) {
+        printf("Unnable to allocate memory");
+        return;
+    }
+
+    new->data = n;
+    new->next = last->next;
+    new->prev = last;
+    last->next = new;
+}
--- DoublyLinkedList/src/main.c
+++ DoublyLinkedList/src/main.c
@@ -1,6 +1,9 @@
 #include "doublylinkedlist.h"
 
+#include <stdio.h>
+
 int main(void) {
-    
+    DoublyList *d = create_list();
+
     return 0;
 }
