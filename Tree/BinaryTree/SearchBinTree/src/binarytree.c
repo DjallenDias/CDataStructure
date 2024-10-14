d4a915d18cbcf8cdae0cb9485c44eedc8ec62511
Djállen Fabrício <djallendias@gmail.com>
2024-10-14 10:09:59 -0300

    Adjusted Binary tree folder

---
+++ Tree/BinaryTree/SearchBinTree/src/binarytree.c
@@ -0,0 +1,57 @@
+#include "binarytree.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct t_knot {
+    struct t_knot * left_c;
+    int info;
+    struct t_knot * right_c;
+};
+
+Knot *create_empty_tree(void) {
+    return NULL;
+}
+
+Knot *create_tree(int n, Knot *lc, Knot *rc) {
+    Knot *new_knot = (Knot *) malloc(sizeof(Knot));
+    new_knot->info = n;
+    new_knot->left_c = lc;
+    new_knot->right_c = rc;
+    return new_knot;
+}
+
+Knot *free_tree(Knot *t) {
+    if(!is_empty(t)) {
+        free_tree(t->left_c);
+        free_tree(t->right_c);
+        free(t);
+    }
+    return NULL;
+}
+
+int is_empty(Knot *t) {
+    return t == NULL;
+}
+
+int max2(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int tree_height(Knot *t) {
+    if (is_empty(t)) {
+        return -1;
+    } else {
+        return 1 + max2(tree_height(t->left_c), tree_height(t->right_c));
+    }
+}
+
+void show_tree(Knot *t) {
+    printf("<");
+    if (!is_empty(t)) {
+        printf("%d ", t->info);
+        show_tree(t->left_c);
+        show_tree(t->right_c);
+    }
+    printf(">");
+}
---
+++ Tree/BinaryTree/SearchBinTree/src/main.c
@@ -0,0 +1,25 @@
+#include "binarytree.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // Left sub-tree
+    Knot *a1 = create_tree(10, NULL, NULL);
+    Knot *a2 = create_tree(9, NULL, a1);
+
+    // Right sub-tree
+    Knot *a3 = create_tree(8, NULL, NULL);
+    Knot *a4 = create_tree(7, NULL, NULL);
+    Knot *a5 = create_tree(6, a3, a4);
+
+    // Main knot
+    Knot *a = create_tree(5, a2, a5);
+
+    show_tree(a);
+    puts("");
+    printf("%d\n", tree_height(a));
+
+    return 0;
+}
