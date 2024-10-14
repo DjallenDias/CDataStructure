db3548cd3c05362d491a442b06f4c1dc8cd08c74
Djállen Fabrício <djallendias@gmail.com>
2024-10-14 10:16:11 -0300

    Implemented show tree Search bin tree

--- Tree/BinaryTree/SearchBinTree/src/searchbinarytree.c
+++ Tree/BinaryTree/SearchBinTree/src/searchbinarytree.c
@@ -9,6 +9,14 @@ struct t_knot {
     struct t_knot * right_c;
 };
 
-Knot *create_knot(void);
+Knot *create_knot(void) {
+    return NULL;
+}
 
-void show_tree(Knot *);
+void show_tree(Knot *tree) {
+    if (tree != NULL) {
+        show_tree(tree->left_c);
+        printf("%d, ", tree->info);
+        show_tree(tree->right_c);
+    }
+}
